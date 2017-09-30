0026afdd7242032e08e27ddc6209fdad87ffa109
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2017-09-30 05:03:10 +0000

    initial commit of dummy image package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dummyimage',
+    version='0.1',
+    description='Download dummy images from dummyimage.com',
+    url='http://github.com/slyboots/dummyimage-cli',
+    author='Slyboots',
+    author_email='admin@slyb--ts.com',
+    license='MIT',
+    packages=['dummyimage'],
+    zip_safe=False,
+    scripts: ['bin/dummyimage']
+)
